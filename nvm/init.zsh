ca60f4274a19125cc90756c28229ec50da02582c
Kevin Hill <kevin.hill@fortunabmc.com>
2023-12-08 06:14:39 -0800

    adding nvm init

---
+++ nvm/init.zsh
@@ -0,0 +1,6 @@
+export NVM_DIR="$HOME/.nvm"
+
+local NVM="/opt/homebrew/opt/nvm"
+
+[ -s "$NVM/nvm.sh" ] && source "$NVM/nvm.sh"  # This loads nvm
+[ -s "$NVM/etc/bash_completion.d/nvm" ] && source "$NVM/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
