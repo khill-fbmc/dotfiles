e6639cede1f9f8a2ec358215cbcd03f3cff8acc6
Kevin Hill <kevin.hill@fortunabmc.com>
2023-12-08 04:50:12 -0800

    updating homebrew install to use bash over ruby

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,17 +8,15 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
-
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  fi
-
+    echo "  Installing Homebrew for you."
+    
+    # Install the correct homebrew for each OS type
+    if test "$(uname)" = "Darwin"
+    then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+        echo "Not on Mac; skipping"
+    fi
 fi
 
 exit 0
