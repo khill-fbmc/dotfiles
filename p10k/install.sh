302d85649a45eadf78fa8139000a9eb91e557f94
Kevin Hill <kevin.hill@fortunabmc.com>
2023-12-08 20:59:36 -0800

    guarding the installer and adding config

--- p10k/install.sh
+++ p10k/install.sh
@@ -1,1 +1,7 @@
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+#!/usr/bin/env zsh
+
+if ! [ -d $HOME/powerlevel10k ]
+then
+    echo "installing powerlevel10k"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+fi
