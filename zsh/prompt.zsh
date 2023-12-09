0940fcd8b6e61a84d3164a1d7f6e6c12fb41eb54
Kevin Hill <kevin.hill@fortunabmc.com>
2023-12-08 20:59:18 -0800

    whoops

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,4 @@
 source $HOME/powerlevel10k/powerlevel10k.zsh-theme
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[ -s "$HOME/.p10k.zsh"] && source "$HOME/.p10k.zsh"
+[ -s "$HOME/.p10k.zsh" ] && source "$HOME/.p10k.zsh"
